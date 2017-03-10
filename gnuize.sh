b2c61d0b3dde8d494e1946cc35367a4042791ab5
Kolen Cheung <christian.kolen@gmail.com>
2017-03-10 11:44:49 -0800

    gnuize.sh: install gcc and set as default compiler for homebrew

--- gnuize.sh
+++ gnuize.sh
@@ -2,6 +2,15 @@
 PROFILE_FILE=$HOME/.bash_profile
 PATH_FILE=$HOME/.bash_path
 
+# multilib is for compiling for other architechture, which may not work with OpenMP
+brew install gcc --without-multilib
+
+# backup .bash_profile if existed
+[ -e "$PROFILE_FILE" ] && mv $PROFILE_FILE $(mktemp ${PROFILE_FILE}.XXXX)
+
+# Use gcc and g++ for packages from homebrew that build from source
+printf "%s\n" "export HOMEBREW_CC=$(find /usr/local/bin -iname "gcc??")" "export HOMEBREW_CXX=$(find /usr/local/bin -iname "g++??")" "" > $PROFILE_FILE
+
 # Install required packages from Homebrew
 brew tap homebrew/dupes
 brew install coreutils binutils diffutils ed findutils gawk gnu-indent gnu-sed \
@@ -10,6 +19,9 @@ brew install coreutils binutils diffutils ed findutils gawk gnu-indent gnu-sed \
   --default-names --with-default-names --with-gettext --override-system-vi \
   --override-system-vim --custom-system-icons
 brew cleanup
+
+# link the python applications installed above to /Applications
+brew linkapps python
 
 # prepare PATH
 # Empty the .bash_path file that holds GNU paths
@@ -32,11 +44,9 @@ done
 printf "%s\n" '${MANPATH+:}$MANPATH"' >> $PATH_FILE
 
 # Check if .bash_path is being called from .bash_profile, if not, rebuild it from scratch
-touch $PROFILE_FILE
 if ! grep -qE "([$]HOME|~)/\.bash_path" $PROFILE_FILE; then
-  [ -e "$PROFILE_FILE" ] && mv $PROFILE_FILE $(mktemp ${PROFILE_FILE}.XXXX)
   # Add Ubuntu-style PS1 to .bash_profile
-  cat <<EOF > $PROFILE_FILE
+  cat <<EOF >> $PROFILE_FILE
 # GNU-ize
 alias ll="ls -ahl --color=always"
 export PS1="\[\033[1;32m\]\u@\h\[\033[0m\]:\[\033[1;34m\]\w\[\033[0m\]# "
