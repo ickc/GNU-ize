b44babff93804496a2a3c277add6900c037d7204
Kolen Cheung <christian.kolen@gmail.com>
2017-08-22 14:45:02 -0700

    gnuize.sh: add gnu-time

--- gnuize.sh
+++ gnuize.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 PROFILE_FILE=$HOME/.bash_profile
 PATH_FILE=$HOME/.bash_path
 
@@ -21,6 +22,7 @@ brew tap homebrew/dupes
 brew install coreutils binutils diffutils ed findutils gawk gnu-indent gnu-sed \
   gnu-tar gnu-which gnutls grep gzip screen watch wdiff wget bash gdb gpatch \
   m4 make nano file-formula git less openssh python rsync svn unzip vim \
+  gnu-time \
   --default-names --with-default-names --with-gettext --override-system-vi \
   --override-system-vim --custom-system-icons
 brew install emacs --with-cocoa --with-gnutls --with-imagemagick@6 --with-librsvg
