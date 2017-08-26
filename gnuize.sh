b33ec9b7385003456fd65e3279003b044fc52764
Kolen Cheung <christian.kolen@gmail.com>
2017-08-25 19:16:21 -0700

    gnuize.sh: inline PROFILE_FILE, PATH_FILE

--- gnuize.sh
+++ gnuize.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
 
-PROFILE_FILE=$HOME/.bash_profile
-PATH_FILE=$HOME/.bash_path
-
 # multilib is for compiling for other architechture, which may not work with OpenMP
 brew install gcc --without-multilib
 
 # backup .bash_profile if existed
-[ -e "$PROFILE_FILE" ] && mv $PROFILE_FILE $(mktemp ${PROFILE_FILE}.XXXX)
+[ -e "$HOME/.bash_profile" ] && mv $HOME/.bash_profile $(mktemp ${$HOME/.bash_profile}.XXXX)
 
 # Use gcc and g++ for packages from homebrew that build from source
 export HOMEBREW_CC=$(find /usr/local/bin -iname "gcc??")
 export HOMEBREW_CC=${HOMEBREW_CC##*/}
 export HOMEBREW_CXX=$(find /usr/local/bin -iname "g++??")
 export HOMEBREW_CXX=${HOMEBREW_CXX##*/}
-# put it in PROFILE_FILE
-printf "%s\n" "export HOMEBREW_CC=$HOMEBREW_CC" "export HOMEBREW_CXX=$HOMEBREW_CXX" "" > $PROFILE_FILE
+# put it in $HOME/.bash_profile
+printf "%s\n" "export HOMEBREW_CC=$HOMEBREW_CC" "export HOMEBREW_CXX=$HOMEBREW_CXX" "" > $HOME/.bash_profile
 
 # Install required packages from Homebrew
 brew tap homebrew/dupes
@@ -33,32 +30,32 @@ brew linkapps python
 
 # prepare PATH
 # Empty the .bash_path file that holds GNU paths
-printf "%s\n" 'export PATH="/usr/local/sbin\' > $PATH_FILE
+printf "%s\n" 'export PATH="/usr/local/sbin\' > $HOME/.bash_path
 # Build PATH variable script in ~/.bash_path
 for i in /usr/local/Cellar/*/*/bin  /usr/local/Cellar/*/*/libexec/gnubin; do
-  printf "%s\n" ":${i//://}\\" >> $PATH_FILE
+  printf "%s\n" ":${i//://}\\" >> $HOME/.bash_path
 done
 #finalize PATH variable (adding back the old PATH, if exists, and closing the qoutes)
-printf "%s\n" '${PATH+:}$PATH"' '' >> $PATH_FILE
+printf "%s\n" '${PATH+:}$PATH"' '' >> $HOME/.bash_path
 
 # prepare MANPATH
 # start the MANPATH
-printf "%s\n" 'export MANPATH="\' >> $PATH_FILE
+printf "%s\n" 'export MANPATH="\' >> $HOME/.bash_path
 # build the contents of MANPATH, the closing quote needs to be within the sed statement
 for i in /usr/local/Cellar/*/*/share/man /usr/local/Cellar/*/*/libexec/gnuman; do
-  printf "%s\n" "$i:\\" >> $PATH_FILE
+  printf "%s\n" "$i:\\" >> $HOME/.bash_path
 done
 #finalize MATHPATH variable (adding back the old MATHPATH, if exists, and closing the qoutes)
-printf "%s\n" '${MANPATH+:}$MANPATH"' >> $PATH_FILE
+printf "%s\n" '${MANPATH+:}$MANPATH"' >> $HOME/.bash_path
 
 # Check if .bash_path is being called from .bash_profile, if not, rebuild it from scratch
-if ! grep -qE "([$]HOME|~)/\.bash_path" $PROFILE_FILE; then
+if ! grep -qE "([$]HOME|~)/\.bash_path" $HOME/.bash_profile; then
   # Add Ubuntu-style PS1 to .bash_profile
-  cat <<EOF >> $PROFILE_FILE
+  cat <<EOF >> $HOME/.bash_profile
 # GNU-ize
 alias ll="ls -ahl --color=always"
 export PS1="\[\033[1;32m\]\u@\h\[\033[0m\]:\[\033[1;34m\]\w\[\033[0m\]# "
-[ -f $PATH_FILE ] && source $PATH_FILE
+[ -f $HOME/.bash_path ] && source $HOME/.bash_path
 
 EOF
 fi
