89539faf542dad236f268bcdcd093fc78937e1b8
Kolen Cheung <christian.kolen@gmail.com>
2017-08-25 20:19:23 -0700

    gnuize.sh: manual flags and paths after inspecting `brew info ...`

--- gnuize.sh
+++ gnuize.sh
@@ -13,51 +13,69 @@ export HOMEBREW_CXX=$(find /usr/local/bin -iname "g++??")
 export HOMEBREW_CXX=${HOMEBREW_CXX##*/}
 
 # Install required packages from Homebrew
-brew install coreutils binutils diffutils ed findutils gawk gnu-indent gnu-sed \
-	gnu-tar gnu-which gnutls grep gzip screen watch wdiff wget bash gdb gpatch \
-	m4 make nano file-formula git less openssh rsync svn unzip vim \
-	gnu-time \
-	--default-names --with-default-names --with-gettext --override-system-vi \
-	--override-system-vim --custom-system-icons
+cat <<EOF | xargs brew install
+bash
+binutils
+coreutils
+diffutils
+file-formula
+gawk
+gdb
+gnutls
+gpatch
+gzip
+less
+m4
+nano
+openssh
+rsync
+screen
+svn
+watch
+wget
+EOF
+
+cat <<EOF | xargs brew install --with-default-names
+ed
+findutils
+gnu-indent
+gnu-sed
+gnu-tar
+gnu-time
+gnu-which
+grep
+make
+EOF
+
+cat <<EOF | xargs brew install --with-gettext
+git
+wdiff
+EOF
+
+brew install vim --with-gettext --with-override-system-vi
 brew install emacs --with-cocoa --with-gnutls --with-imagemagick@6 --with-librsvg
+
 brew cleanup
 
 # .bash_path ###########################################################
 
-# prepare PATH
-# Empty the .bash_path file that holds GNU paths
-printf "%s\n" 'export PATH="/usr/local/sbin\' > $HOME/.bash_path
-# Build PATH variable script in ~/.bash_path
-for i in /usr/local/Cellar/*/*/bin	/usr/local/Cellar/*/*/libexec/gnubin; do
-	printf "%s\n" ":${i//://}\\" >> $HOME/.bash_path
-done
-#finalize PATH variable (adding back the old PATH, if exists, and closing the qoutes)
-printf "%s\n" '${PATH+:}$PATH"' '' >> $HOME/.bash_path
+# default to use gcc
+printf "%s\n" "# homebrew compiling using gcc" "export HOMEBREW_CC=$HOMEBREW_CC" "export HOMEBREW_CXX=$HOMEBREW_CXX" "" > $HOME/.bash_path
 
-# prepare MANPATH
-# start the MANPATH
-printf "%s\n" 'export MANPATH="\' >> $HOME/.bash_path
-# build the contents of MANPATH, the closing quote needs to be within the sed statement
-for i in /usr/local/Cellar/*/*/share/man /usr/local/Cellar/*/*/libexec/gnuman; do
-	printf "%s\n" "$i:\\" >> $HOME/.bash_path
-done
-#finalize MATHPATH variable (adding back the old MATHPATH, if exists, and closing the qoutes)
-printf "%s\n" '${MANPATH+:}$MANPATH"' >> $HOME/.bash_path
+cat <<EOF >> $HOME/.bash_path
+# Ubuntu-style PS1
+alias ll="ls -ahl --color=always"
+export PS1="\[\033[1;32m\]\u@\h\[\033[0m\]:\[\033[1;34m\]\w\[\033[0m\]# "
+# cellar path
+export PATH="/usr/local/opt/file-formula/bin:\$PATH"
+export PATH="/usr/local/opt/m4/bin:\$PATH"
+export PATH="/usr/local/opt/unzip/bin:\$PATH"
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:\$PATH"
+export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:\$MANPATH"
+EOF
 
 # .bash_profile ########################################################
 
-# default to use gcc
-printf "%s\n" "" "# homebrew compiling using gcc" "export HOMEBREW_CC=$HOMEBREW_CC" "export HOMEBREW_CXX=$HOMEBREW_CXX" "" >> $HOME/.bash_profile
-
-# Add Ubuntu-style PS1 to .bash_profile
-cat <<EOF >> $HOME/.bash_profile
-# GNU-ize
-alias ll="ls -ahl --color=always"
-export PS1="\[\033[1;32m\]\u@\h\[\033[0m\]:\[\033[1;34m\]\w\[\033[0m\]# "
-[ -f $HOME/.bash_path ] && source $HOME/.bash_path
-EOF
-
-# rather than creating a new bash profile, or trying to be smart when writing the bash profile, ask the user to cleanup any potential conflicts.
-# because the gcc version might be different and difficult to detect. Moreover, these commands are not harmful to be loaded more than once.
-echo "Please cleanup the bash profile."
-open $HOME/.bash_profile
+if ! grep -qE "([$]HOME|~)/\.bash_path" $HOME/.bash_profile; then
+	printf "%s\n" "" '[ -f $HOME/.bash_path ] && . $HOME/.bash_path' "" >> $HOME/.bash_profile
+fi
